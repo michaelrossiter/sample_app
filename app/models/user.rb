c77b8025b421d41a1bce226926bf36cfd0fd29a1
Michael <michael.b.s.rossiter@gmail.com>
2014-05-23 13:28:32 -0700

    building following-users module

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_it", class_name: "Relationship", dependent: :destroy 
+  has_many :followers, through: :reverse_relationships, source: :follower
+
+
   before_save { self.email = email.downcase }
   before_create :create_remember_token
   validates :name,  presence: true, length: { maximum: 50 }
@@ -12,6 +18,7 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }  
 
 
+# Authentication methods
   def User.new_remember_token
   	SecureRandom.urlsafe_base64
   end
@@ -20,12 +27,26 @@ class User < ActiveRecord::Base
   	Digest::SHA1.hexdigest(token.to_s)
   end
 
+# Feed methods
   def feed
     # This is preliminary, we will revisit
     Micropost.where("user_id = ?", id)
   end
 
+# Relationship methods
+  def following?(other_user)
+    self.relationships.find_by(followed_id: other_user.id)
+  end
 
+  def follow!(other_user)
+    self.relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    self.relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+# Private methods
   private
 	  def create_remember_token
 	  	self.remember_token = User.digest(User.new_remember_token)
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -33,6 +33,16 @@ describe "Static pages" do
           expect(page).to have_selector("li##{item.id}", text: item.content)
         end
       end
+      
+      it "should show the count of following and followers" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
     end
  end
 
