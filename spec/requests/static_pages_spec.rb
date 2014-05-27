af7502888ecf86f336b12ed466f2db7f7050aa2e
Michael <michael.b.s.rossiter@gmail.com>
2014-05-27 13:16:16 -0700

    messing with tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -17,10 +17,11 @@ describe "Static pages" do
     it "should not have a custom page title" do
       visit '/'
        expect(page).not_to have_title("| Home")
-   end
- 
-   describe "for signed-in users" do
+    end
+  
+    describe "for signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
+      let(:other_user) { FactoryGirl.create(:user) }
       before do
         FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
         FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
@@ -35,7 +36,6 @@ describe "Static pages" do
       end
       
       it "should show the count of following and followers" do
-        let(:other_user) { FactoryGirl.create(:user) }
         before do
           other_user.follow!(user)
           visit root_path
@@ -43,23 +43,41 @@ describe "Static pages" do
 
         it { should have_link("0 following", href: following_user_path(user)) }
         it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
     end
- end
+  end
 
-
- describe "Help page" do
+  describe "Help page" do
     it "should have the content 'Help'" do
      visit '/help'
       expect(page).to have_content('Help')
-   end
+    end
   end
 
 
- describe "About page" do
+  describe "About page" do
    it "should have the content 'About Us'" do
     visit '/about'
      expect(page).to have_content('About Us')
     end
-   end
+  end
 
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_title(full_title('Contact')) }
+  end
 end
